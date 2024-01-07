97610c769ba7997b06641aa57ef9875385b0f443
Nathan Miguel <nathan.cm77@gmail.com>
2024-01-07 17:52:39 -0300

    Updated Packages

--- cmake/deps/vulkan.cmake
+++
@@ -1,9 +0,0 @@
-find_package(Vulkan REQUIRED)
-if(Vulkan_FOUND)
-    message(STATUS "Vulkan target include directories: ${Vulkan_INCLUDE_DIRS}")
-    message(STATUS "Vulkan libraries directories: ${Vulkan_LIBRARIES}")
-
-else()
-    message(FATAL_ERROR "You have to install vulkan sdk from 'https://vulkan.lunarg.com/sdk/home'.")
-
-endif()
